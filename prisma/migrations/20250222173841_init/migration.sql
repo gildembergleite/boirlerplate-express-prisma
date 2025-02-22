0f1753268ff667189c9c32966d034ad50c3c03ec
gildembergleite <gildembergleite@gmail.com>
2025-02-22 15:01:42 -0300

    feat: creted auth flow

---
+++ prisma/migrations/20250222173841_init/migration.sql
@@ -0,0 +1,37 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "avatar" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Link" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "postedByUserId" TEXT NOT NULL,
+    CONSTRAINT "Link_postedByUserId_fkey" FOREIGN KEY ("postedByUserId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "RefreshToken" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "tokenHash" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "expiresAt" DATETIME NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "RefreshToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Link_url_key" ON "Link"("url");
--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -1,12 +1,40 @@
 import { Request, Response } from 'express'
+import jwt from 'jsonwebtoken'
 import { UserService } from '../services/user-service'
+import { prisma } from '../config/prisma-client'
 
 export class AuthController {
   static async login(req: Request, res: Response) {
     try {
       const { username, password } = req.body
+
       const user = await UserService.authenticateUser(username, password)
-      res.json(user)
+
+      const accessToken = jwt.sign(
+        { id: user.id },
+        process.env.ACCESS_TOKEN_SECRET as string,
+        { expiresIn: '15m' }
+      )
+
+      const refreshToken = jwt.sign(
+        { id: user.id },
+        process.env.REFRESH_TOKEN_SECRET as string,
+        { expiresIn: '7d' }
+      )
+
+      await prisma.refreshToken.create({
+        data: {
+          tokenHash: refreshToken,
+          userId: user.id,
+          expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
+        }
+      })
+
+      res.json({
+        user,
+        accessToken,
+        refreshToken
+      })
     } catch (err) {
       const error = err as Error
       res.status(401).json({ message: error.message })
